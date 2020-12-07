95d645926b62964b2256b3d1698ce63459874063
Taylor M <60191576+tmcvittie1153@users.noreply.github.com>
2020-12-07 15:49:18 -0500

    APK released

--- app/src/main/java/ca/on/conesotgac/te/snakeeyes/MainActivity.java
+++ app/src/main/java/ca/on/conesotgac/te/snakeeyes/MainActivity.java
@@ -18,7 +18,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
